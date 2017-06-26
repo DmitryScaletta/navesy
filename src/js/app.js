f83d170bec24a5cd581ecae18b83d9c046eed4fb
DmitryScaletta <dmitryscaletta@mail.ru>
2017-06-26 23:45:31 +0300

    Added reviews slider and popup

---
+++ src/js/app.js
@@ -0,0 +1,51 @@
+import Swipe from 'swipejs';
+
+
+// popup
+(() => {
+  const popup = document.querySelector('.js-popup');
+
+  if (popup === null) return;
+
+  const togglePopup = () => popup.classList.toggle('js-popup-visible');
+
+  window.addEventListener('click', (e) => {
+    if (e.target === popup) togglePopup();
+  });
+
+  Array.prototype.forEach.call(
+    document.querySelectorAll('.js-popup-toggle'),
+    elem => elem.addEventListener('click', (e) => {
+      togglePopup();
+      e.preventDefault();
+    }),
+  );
+})();
+
+
+// reviews slider
+(() => {
+  const sliderContainer = document.querySelector('.slider-reviews');
+
+  if (sliderContainer === null) return;
+
+  const dots = sliderContainer.querySelectorAll('.js-slider-dots span');
+
+  const clearDots = () => Array.prototype.forEach.call(dots, dot => dot.classList.remove('active'));
+  const setCurrentDot = index => dots[index].classList.add('active');
+
+  const slider = new Swipe(sliderContainer, {
+    draggable: true,
+    callback: (index) => {
+      clearDots();
+      setCurrentDot(index);
+    },
+  });
+
+  Array.prototype.forEach.call(dots, (dot, index) => dot.addEventListener('click', () => {
+    clearDots();
+    setCurrentDot(index);
+    slider.slide(index);
+  }));
+})();
+
